e5845fd0a13941f1e14486a14dc44434f7865ba4
Kyle Angeles <kyle.angeles@dcmail.ca>
2026-06-20 15:54:56 -0400

    moving along getting into the major learning material

--- SciKitLearn/main.py
+++ SciKitLearn/main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+# This library just helps with splitting up the data set
+from sklearn.model_selection import train_test_split
+# Classification model import library 
+from sklearn.ensemble import RandomForestClassifier
+
+
 # Preparing data through csv file or creating a table manually
 heart_disease = pd.read_csv("https://raw.githubusercontent.com/mrdbourke/zero-to-mastery-ml/master/data/heart-disease.csv")
 print(heart_disease.head())
@@ -14,3 +20,13 @@ print(X.head())
 
 print(Y.head(), Y.value_counts())
 
+
+# Splitting the data into training and test sets (in simple terms training is studying and test is exam which will be the final set)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
+
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+
+# Current hyperparams settings
+clf = RandomForestClassifier()
+print(clf.get_params())
