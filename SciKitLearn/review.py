af3c2e69e711cd320dcd0e5b66f64ea73a1e3b75
Kyle Angeles <kyle.angeles@dcmail.ca>
2026-06-20 20:27:41 -0400

    onto what im reviewing based off the chapters and rereading the tutorials for more in depth insight

---
+++ SciKitLearn/review.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+car_sales = pd.read_csv("https://raw.githubusercontent.com/mrdbourke/zero-to-mastery-ml/master/data/car-sales-extended.csv")
+print(car_sales)
