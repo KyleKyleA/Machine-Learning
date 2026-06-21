30f2f8040e62789887cd1fb4d992c4359ca1fc29
Kyle Angeles <kyle.angeles@dcmail.ca>
2026-06-20 20:56:32 -0400

    fixed the bug all i had to do was move some code and uncommeted myt code

--- SciKitLearn/review.py
+++ SciKitLearn/review.py
@@ -17,8 +17,52 @@ print(X_train.shape, X_test.shape, Y_train.shape, Y_test.shape)
 # Spliting data 
 print(X.shape[0] * 0.8)
 print(X.shape[0] * 0.2)
+##############################################################################
 
+# Car sales dataset 
+from sklearn.ensemble import RandomForestClassifier
+
+# Theses two libraries help convert string values from a table into a numerical since machine learning prefers that
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 
 car_sales = pd.read_csv("https://raw.githubusercontent.com/mrdbourke/zero-to-mastery-ml/master/data/car-sales-extended.csv")
 print(car_sales)
+print(car_sales.dtypes)
+
+categorical_features = ["Make", "Colour", "Doors"] 
+
+X = car_sales.drop("Price", axis=1)
+y = car_sales['Price']
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
+
+one_hot = OneHotEncoder()
+
+# creating an instance for the transformer 
+transformer = ColumnTransformer([("one_hot",
+                                  one_hot,
+                                  categorical_features,
+                                  )],
+                                remainder="passthrough")
+
+# turn the features that were valued string into numerical values that will return into byte matrices codes
+transformed_X = transformer.fit_transform(X)
+print(transformed_X)
+
+print(X.head())
+
+# Example of printing the index of 0 into transformed integer values that can get crazy
+print(transformed_X[0])
+
+
+
+# Splitting the data below
+
+
+
+# model = RandomForestRegressor()
+# model.fit(X_train, Y_train)
+# model.score(X_test, Y_test)
+
+
